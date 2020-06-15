07ff18288f0e419a61640447574487255b841610
Wottan32 <mariotorreslagos@gmail.com>
2020-06-15 16:11:10 -0300

    readme2
    2d upload

---
+++ Empresas.Negocio/Empleado.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Empresas.Datos;
+
+namespace Empresas.Negocio
+{
+    public class Empleado
+    {
+        /// <summary>
+        /// Retorna o asigna el Rut
+        /// </summary>
+        public int Rut { get; set; }
+
+        /// <summary>
+        /// Retorna o asigna el Dígito Verificador
+        /// </summary>
+        public string Dv { get; set; }
+
+        /// <summary>
+        /// Retorna o asigna los Nombres
+        /// </summary>
+        public string Nombres { get; set; }
+
+        /// <summary>
+        /// Retorna o asigna los Apellidos
+        /// </summary>
+        public string Apellidos { get; set; }
+
+        /// <summary>
+        /// Retorna o asigna el Rut de la Empresa a la que pertenece
+        /// </summary>
+        public int RutEmpresa { get; set; }
+
+        /// <summary>
+        /// Constructor por defecto de la clase Empleado
+        /// </summary>
+        public Empleado()
+        {
+            this.Init();
+        }
+
+        /// <summary>
+        /// Inicializa instacias privadas y propiedades
+        /// </summary>
+        private void Init()
+        {
+            Rut = 0;
+            Dv = string.Empty;
+            Nombres = string.Empty;
+            Apellidos = string.Empty;
+            RutEmpresa = 0;
+        }
+
+        /// <summary>
+        /// Crea un nuevo registro de Empleado
+        /// </summary>
+        /// <returns></returns>
+        public bool Create()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+            Datos.Empleado emp = new Datos.Empleado();
+
+            try
+            {
+                CommonBC.Syncronize(this, emp);
+
+                bbdd.Empleado.Add(emp);
+                bbdd.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                bbdd.Empleado.Remove(emp);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Lee un registro de Empleado
+        /// </summary>
+        /// <returns></returns>
+        public bool Read()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+
+            try
+            {
+                Datos.Empleado emp = bbdd.Empleado.First(e => e.Rut == Rut);
+
+                CommonBC.Syncronize(emp , this);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Actualiza un registro de Empleado
+        /// </summary>
+        /// <returns></returns>
+        public bool Update()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+
+            try
+            {
+                Datos.Empleado emp = bbdd.Empleado.First(e => e.Rut == Rut);
+
+                CommonBC.Syncronize(this, emp);
+
+                bbdd.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Elimna un registro de Empleado
+        /// </summary>
+        /// <returns></returns>
+        public bool Delete()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+
+            try
+            {
+                Datos.Empleado emp = bbdd.Empleado.First(e => e.Rut == Rut);
+
+                bbdd.Empleado.Remove(emp);
+                bbdd.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Lee todos los registros de Empleado
+        /// </summary>
+        /// <returns></returns>
+        public List<Empleado> ReadAll()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+
+            try
+            {
+                List<Datos.Empleado> listaDatos = bbdd.Empleado.ToList<Datos.Empleado>();
+
+                List<Empleado> listaNegocio = GenerarListado(listaDatos);
+
+                return listaNegocio;
+
+            }
+            catch (Exception ex)
+            {
+                return new List<Empleado>();
+            }
+        }
+
+        private List<Empleado> GenerarListado(List<Datos.Empleado> listaDatos)
+        {
+            List<Empleado> listaNegocio = new List<Empleado>();
+
+            foreach (Datos.Empleado dato in listaDatos)
+            {
+                Empleado negocio = new Empleado();
+                CommonBC.Syncronize(dato, negocio);
+
+                listaNegocio.Add(negocio);
+            }
+
+
+            return listaNegocio;
+        }
+    }
+}
---
+++ Empresas.Negocio/Empresa.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Empresas.Datos;
+
+namespace Empresas.Negocio
+{
+    /// <summary>
+    /// Representa los datos de una Empresa
+    /// </summary>
+    public class Empresa
+    {
+        /// <summary>
+        /// Retorna o asigna el Rut
+        /// </summary>
+        public int Rut { get; set; }
+
+        /// <summary>
+        /// Retorna o asigna el Dígito Verificador
+        /// </summary>
+        public string Dv { get; set; }
+
+        /// <summary>
+        /// Retorna o asigna la Razón Social
+        /// </summary>
+        public string RazonSocial { get; set; }
+
+        /// <summary>
+        /// Constructor por defecto de la clase EMpresa
+        /// </summary>
+        public Empresa()
+        {
+            this.Init();
+        }
+
+        /// <summary>
+        /// Inicializa las instancias privadas de la clase 
+        /// </summary>
+        private void Init()
+        {
+            Rut = 0;
+            Dv = string.Empty;
+            RazonSocial = string.Empty;
+        }
+
+        /* OPERACIONES DE MANTENCION */
+
+        /// <summary>
+        /// Crea un registro de empresa en la BBDD
+        /// </summary>
+        /// <returns></returns>
+        public bool Create()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+
+            Datos.Empresa emp = new Datos.Empresa();
+
+            try
+            {
+                CommonBC.Syncronize(this, emp);
+
+                bbdd.Empresa.Add(emp);
+                bbdd.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                bbdd.Empresa.Remove(emp);
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Lee un registro de empresa en la BBDD
+        /// </summary>
+        /// <returns></returns>
+        public bool Read()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+
+            try
+            {
+                /* Se obtiene el primer registro coincidente con el Rut */
+                Datos.Empresa emp = bbdd.Empresa.First( e => e.Rut == Rut);
+
+                /* Se copian las propiedades de datos al negocio */
+                CommonBC.Syncronize(emp, this);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Actualiza un registro de empresa en la BBDD
+        /// </summary>
+        /// <returns></returns>
+        public bool Update()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+
+            try
+            {
+                /* Se obtiene el primer registro coincidente con el Rut */
+                Datos.Empresa emp = bbdd.Empresa.First(e => e.Rut == Rut);
+
+                /* Se copian las propiedades del negocio a los datos */
+                CommonBC.Syncronize(this, emp);
+
+                bbdd.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Elimina un registro de empresa en la BBDD
+        /// </summary>
+        /// <returns></returns>
+        public bool Delete()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+
+            try
+            {
+                /* Se obtiene el primer registro coincidente con el Rut */
+                Datos.Empresa emp = bbdd.Empresa.First(e => e.Rut == Rut);
+
+                /* Se elimina el registro del EDM */
+                bbdd.Empresa.Remove(emp);
+
+                bbdd.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Lee todos los registros de empresa
+        /// </summary>
+        /// <returns></returns>
+        public List<Empresa> ReadAll()
+        {
+            Datos.EmpresasEntities bbdd = new Datos.EmpresasEntities();
+
+            try
+            {
+                /* Se obtiene todos los registro desde la tabla */
+                List<Datos.Empresa> listadoDatos = bbdd.Empresa.ToList<Datos.Empresa>();
+
+                /* Se convierte el listado de datos en un listado de negocio */
+                List<Empresa> listadoNegocio = GenerarListado(listadoDatos);
+
+                /* Se retorna la lista */
+                return listadoNegocio;
+            }
+            catch (Exception ex)
+            {
+                return new List<Empresa>();
+            }
+        }
+
+        /// <summary>
+        /// Convierte un listado de objetos de datos en un listado de objetos de negocio
+        /// </summary>
+        /// <param name="listadoDatos"></param>
+        /// <returns></returns>
+        private List<Empresa> GenerarListado(List<Datos.Empresa> listadoDatos)
+        {
+            List<Empresa> listadoEmpresa = new List<Empresa>();
+
+            foreach (Datos.Empresa dato in listadoDatos)
+            {
+
+                Empresa negocio = new Empresa();
+                CommonBC.Syncronize(dato, negocio);
+
+                listadoEmpresa.Add(negocio);
+            }
+
+            return listadoEmpresa;
+        }
+    }
+
+}
+
